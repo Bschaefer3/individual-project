c40045fb878bae426f90539a6622e7e48266ad31
bschaefer3 <bschaefer3@madisoncollege.edu>
2020-04-30 19:46:34 -0500

    Added api.properties and changed itemsDao to use properties file

--- src/main/java/poe/persistence/ItemsDao.java
+++ src/main/java/poe/persistence/ItemsDao.java
@@ -10,20 +10,49 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ItemsDao {
 
-    Client client;
+    private Properties properties;
+    private Client client;
+    private String api;
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     ItemsDao() {
         client = ClientBuilder.newClient();
+        api = loadProperties();
+
     }
 
+    /**
+     * Loads the properties from the properties file
+     */
+    private String loadProperties() {
+        properties = new Properties();
+        try {
+            properties.load (this.getClass().getResourceAsStream("/api.properties"));
+        } catch (IOException ioe) {
+            logger.error("Database.loadProperties()...Cannot load the properties file");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            logger.error("Database.loadProperties()..." + e);
+            e.printStackTrace();
+        }
+
+        return properties.getProperty("api");
+    }
+
+    /**
+     * Grabs the item data from the POE API
+     * @param itemId ID of the item
+     * @return All of the information about the item
+     */
     public Items getItem(int itemId) {
-        Client client = ClientBuilder.newClient();
+        client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("https://api.poe.watch/item?id=" + itemId);
+                client.target(api + itemId);
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         Items item = new Items();
 
